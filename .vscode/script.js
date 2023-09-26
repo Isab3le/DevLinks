50312a7920f6e3d4821945f46fc84b5e816b790f
Isabele Souza <isabele.engmec@gmail.com>
2023-09-26 18:50:25 -0300

    add Staged Changes

--- .vscode/script.js
+++ .vscode/script.js
@@ -12,3 +12,15 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar.png");
   }
 }
+
+function share() {
+  if (navigator.share !== undefined) {
+    navigator
+      .share({
+        title: "DevLinks",
+        url: "file:///C:/Users/beles/Desktop/projeto/.vscode/index.html",
+      })
+      .then(() => console.log("Successful share"))
+      .catch((error) => console.log("Error sharing", error));
+  }
+}
